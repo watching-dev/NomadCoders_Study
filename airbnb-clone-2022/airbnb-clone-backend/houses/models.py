4ebbe9dee88a66458abb5a790fa43709811efc0b
watching-dev <coderefactoring90@gmail.com>
2023-10-01 20:46:21 +0900

    [feat] admin 페이지 리스트 이름 admin object -> name 으로 변경

--- airbnb-clone-2022/airbnb-clone-backend/houses/models.py
+++ airbnb-clone-2022/airbnb-clone-backend/houses/models.py
@@ -14,3 +14,7 @@ class House(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        
+        return self.name
