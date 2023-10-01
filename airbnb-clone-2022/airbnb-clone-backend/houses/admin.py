9080939f9effd873dff3f4c4ae9a45369d7b0dc1
watching-dev <coderefactoring90@gmail.com>
2023-10-01 20:50:24 +0900

    [feat] admin 페이지 column 추가(list_display)

--- airbnb-clone-2022/airbnb-clone-backend/houses/admin.py
+++ airbnb-clone-2022/airbnb-clone-backend/houses/admin.py
@@ -4,4 +4,11 @@ from .models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = [
+
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed"
+    ]
