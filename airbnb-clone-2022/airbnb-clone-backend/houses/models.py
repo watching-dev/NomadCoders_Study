fe3295bfb357729e373f2d40b5516211947029da
watching-dev <coderefactoring90@gmail.com>
2023-10-03 20:40:23 +0900

    [feat] 마이그레이션 및 슈퍼 유저 추가

--- airbnb-clone-2022/airbnb-clone-backend/houses/models.py
+++ airbnb-clone-2022/airbnb-clone-backend/houses/models.py
@@ -17,7 +17,7 @@ class House(models.Model):
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True)
 
-    owner = models.ForeignKey("users.User", on_delelte=models.CASCADE)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
