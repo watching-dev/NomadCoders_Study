bf2706c93598cb5274377542d5d40f6f00f4bb39
watching-dev <coderefactoring90@gmail.com>
2023-10-05 20:13:45 +0900

    [feat] rooms 에러, 컨벤션  해결

--- airbnb-clone-2022/airbnb-clone-backend/config/settings.py
+++ airbnb-clone-2022/airbnb-clone-backend/config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 CUSTOM_APPS = [
     "users.apps.UsersConfig",
-    "rooms.apps.UserConfig",
+    "rooms.apps.RoomsConfig",
 ]
 
 SYSTEM_APPS = [
--- airbnb-clone-2022/airbnb-clone-backend/rooms/models.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/models.py
@@ -7,36 +7,36 @@ class Room(models.Model):
     """Room Model Definition"""
 
     class RoomKindChoices(models.TextChoices):
-        ENTIRE_PLACE = ("entire_place", "entire_place")
-        PRIVATE_ROOM = ("private_room", "private_room")
-        SHARED_ROOM = "shred_room", "shared_room"
+        ENTIRE_PLACE = ("entire_place", "entire place")
+        PRIVATE_ROOM = ("private_room", "private room")
+        SHARED_ROOM = "shared_room", "shared room"
 
-        country = models.CharField(
-            max_length=50,
-            default="한국",
-        )
-        city = models.CharField(
-            max_length=80,
-            default="서울",
-        )
-        price = models.PositiveIntegerField()
-        rooms = models.PositiveIntegerField()
-        toilets = models.PositiveIntegerField()
-        description = models.TextField()
-        address = models.CharField(
-            max_length=250,
-        )
-        pet_fridendly = models.BooleanField(
-            default=True,
-        )
-        kind = models.CharField(
-            max_length=20,
-            choices=RoomKindChoices.choices,
-        )
-        owner = models.ForeignKey(
-            "users.User",
-            on_delete=models.CASCADE,
-        )
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(
+        max_length=250,
+    )
+    pet_fridendly = models.BooleanField(
+        default=True,
+    )
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
 
     class Amenity(models.Model):
 
