84336e575117dd3bd1619b3463f4e5a45e831033
watching-dev <coderefactoring90@gmail.com>
2023-10-05 20:07:20 +0900

    [feat] rooms 애플리케이션, 모델 추가

---
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+# Create your models here.
+class Room(models.Model):
+
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "entire_place")
+        PRIVATE_ROOM = ("private_room", "private_room")
+        SHARED_ROOM = "shred_room", "shared_room"
+
+        country = models.CharField(
+            max_length=50,
+            default="한국",
+        )
+        city = models.CharField(
+            max_length=80,
+            default="서울",
+        )
+        price = models.PositiveIntegerField()
+        rooms = models.PositiveIntegerField()
+        toilets = models.PositiveIntegerField()
+        description = models.TextField()
+        address = models.CharField(
+            max_length=250,
+        )
+        pet_fridendly = models.BooleanField(
+            default=True,
+        )
+        kind = models.CharField(
+            max_length=20,
+            choices=RoomKindChoices.choices,
+        )
+        owner = models.ForeignKey(
+            "users.User",
+            on_delete=models.CASCADE,
+        )
+
+    class Amenity(models.Model):
+
+        """Amenity Definition"""
+
+        name = models.CharField(
+            max_length=150,
+        )
+        description = models.CharField(
+            max_length=150,
+            null=True,
+        )
