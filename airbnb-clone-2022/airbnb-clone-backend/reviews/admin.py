5198b406d786563bcc72038ca6ad3a72ba9d95df
watching-dev <coderefactoring90@gmail.com>
2023-10-07 19:47:51 +0900

    [feat] reviews 애플리케이션 추가 및 마이그레이션

---
+++ airbnb-clone-2022/airbnb-clone-backend/reviews/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "payload",
+    )
+
+    list_filter = ("rating",)
