c8e3decc76410bae1de8214d0107944f4972552f
watching-dev <coderefactoring90@gmail.com>
2023-10-07 18:10:08 +0900

    [doc] 새로운 환경일때 필요한 pillow 추가 및 환경설정

--- airbnb-clone-2022/airbnb-clone-backend/README.md
+++ airbnb-clone-2022/airbnb-clone-backend/README.md
@@ -13,11 +13,13 @@
 
 > > > > (가상환경에 들어가서 장고 시작) -> poetry shell
 
-> > > > > (에러가 나고 admin 페이지에 접속 안될텐데 마이그레이션\_db의 state 변경 해야해서 그럼) -> control + c 로 서버 끄고 -> python manage.py migrate
+> > > > > (에러가 나고 admin 페이지에 접속 안될텐데 마이그레이션_db의 state 변경 해야해서 그럼) -> control + c 로 서버 끄고 -> python manage.py migrate -> (만약 안된다면 마이그레이션, 디비 다 삭제하고) -> python manage.py makemigrations -> python manage.py migrate
 
 > > > > > > (admin 페이지 로그인 하려면 슈퍼 유저 만들어야함) -> python manage.py createsuperuser -> user name, email 그냥 엔터x2 -> 비밀번호는 그냥 123456 -> y
 
 > > > > > > > 이 순서로 해야 정상 작동함(물론 기존 파일과 다른점 있는지는 확인 해야함) / 데이터 베이스는 깃에 없으니 다시 슈퍼유저 만들어야 하는거고!
+
+> > > > > > > > (image를 가져오기 위해서 pillow 설치) -> poetry add pillow
 
 ---
 
--- airbnb-clone-2022/airbnb-clone-backend/experiences/migrations/0002_initial.py
+++ airbnb-clone-2022/airbnb-clone-backend/experiences/migrations/0002_initial.py
@@ -10,14 +10,19 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('rooms', '0001_initial'),
+        ('experiences', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='room',
-            name='owner',
+            model_name='experience',
+            name='host',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='experience',
+            name='perks',
+            field=models.ManyToManyField(to='experiences.perk'),
         ),
     ]
