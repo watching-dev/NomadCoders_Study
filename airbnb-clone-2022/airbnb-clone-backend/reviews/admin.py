ada193598c725d66fe3fc6a14c63400e04a7660a
watching-dev <coderefactoring90@gmail.com>
2023-10-09 19:04:26 +0900

    [feat] custom filter 추가

--- airbnb-clone-2022/airbnb-clone-backend/reviews/admin.py
+++ airbnb-clone-2022/airbnb-clone-backend/reviews/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 # Register your models here.
 class ReviewAdmin(admin.ModelAdmin):
@@ -10,4 +30,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "payload",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
