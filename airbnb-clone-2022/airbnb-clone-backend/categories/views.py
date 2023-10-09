96f7ff20709aced2b1cbd40e27f1f248a85c5a95
watching-dev <coderefactoring90@gmail.com>
2023-10-09 21:15:21 +0900

    [feat] 장고 rest 프레임워크 사용_api_view

--- airbnb-clone-2022/airbnb-clone-backend/categories/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/categories/views.py
@@ -1,17 +1,13 @@
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 # Create your views here.
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "ok": True,
-            "categories": serializers.serialize(
-                "json",
-                all_categories,
-            ),
         },
     )
