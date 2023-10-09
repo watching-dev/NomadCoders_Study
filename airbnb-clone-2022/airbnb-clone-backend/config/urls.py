0a4346b3db7818543ca09cbb753127e3485a1f83
watching-dev <coderefactoring90@gmail.com>
2023-10-09 19:37:08 +0900

    [feat] config urls -> rooms urls

--- airbnb-clone-2022/airbnb-clone-backend/config/urls.py
+++ airbnb-clone-2022/airbnb-clone-backend/config/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rooms.views import say_hello
-from users import views as users_views
-from bookings import views as bookings_views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms/", say_hello),
+    path("rooms/", include("rooms.urls")),
 ]
