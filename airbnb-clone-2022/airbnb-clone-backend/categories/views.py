9a3e3877f2259921e3348d4a91d3151ea874c8f0
watching-dev <coderefactoring90@gmail.com>
2023-10-15 01:37:12 +0900

    [feat] delete 추가 및 url을 api 형식으로 변경(아마추어 같은 느낌 없애도록)

--- airbnb-clone-2022/airbnb-clone-backend/categories/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/categories/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
 from .serializer import CategorySerializer
 
@@ -23,7 +24,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -47,3 +48,7 @@ def category(request, pk):
 
         else:
             return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(HTTP_204_NO_CONTENT)
