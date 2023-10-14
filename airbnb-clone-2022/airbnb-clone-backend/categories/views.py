e25dd72fc1d671149e3c06462ac1c55380af35f8
watching-dev <coderefactoring90@gmail.com>
2023-10-14 23:32:23 +0900

    [feat] serializer 추가_jason 커스텀

--- airbnb-clone-2022/airbnb-clone-backend/categories/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/categories/views.py
@@ -1,13 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializer import CategorySerializer
 
 
 @api_view()
 # Create your views here.
 def categories(request):
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
     return Response(
         {
             "ok": True,
+            "categories": serializer.data,
         },
     )
