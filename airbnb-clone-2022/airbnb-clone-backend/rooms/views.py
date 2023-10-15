b74abdd6241f47fe0ac0be0cd57bb02ce7e37122
watching-dev <coderefactoring90@gmail.com>
2023-10-15 20:31:15 +0900

    [feat] REST API_리팩토링_All Amenities

--- airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Amenity
+from .serializers import AmenitySerializer
+
+
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(
+            all_amenities,
+            many=True,
+        )
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(
+                AmenitySerializer(amenity).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
+
+
+""" from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Room
 
@@ -31,4 +69,4 @@ def see_one_room(request, room_pk):
             request,
             "room.detail.html",
             {"not_found": True},
-        )
+        ) """
