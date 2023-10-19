faef74a9be3eb8c6fbca9ab9c1f54bfc2a96fd79
watching-dev <coderefactoring90@gmail.com>
2023-10-20 00:40:37 +0900

    [feat] RoomDetailSerializer 추가

--- airbnb-clone-2022/airbnb-clone-backend/categories/serializer.py
+++ airbnb-clone-2022/airbnb-clone-backend/categories/serializer.py
@@ -5,23 +5,7 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
-
-    """ pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(read_only=True)
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kine", instance.kind)
-        instance.save()
-        return instance """
+        fields = (
+            "name",
+            "kind",
+        )
