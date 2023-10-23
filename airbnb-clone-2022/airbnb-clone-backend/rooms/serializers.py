a317eab145cb8f8bce230f52324536829a942c28
watching-dev <coderefactoring90@gmail.com>
2023-10-23 19:34:59 +0900

    [feat] Serializer Context 추가

--- airbnb-clone-2022/airbnb-clone-backend/rooms/serializers.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/serializers.py
@@ -27,6 +27,7 @@ class RoomDetailSerializer(ModelSerializer):
     )
 
     rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -35,9 +36,14 @@ class RoomDetailSerializer(ModelSerializer):
     def get_rating(self, room):
         return room.rating()
 
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
+
 
 class RoomListSerializer(ModelSerializer):
-    rating = serializers.SerializerMethodField
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -48,7 +54,12 @@ class RoomListSerializer(ModelSerializer):
             "city",
             "price",
             "rating",
+            "is_owner",
         )
 
     def get_rating(self, room):
         return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
--- airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
@@ -74,7 +74,11 @@ class AmenityDetail(APIView):
 class Rooms(APIView):
     def get(self, request):
         all_rooms = Room.objects.all()
-        serializer = RoomListSerializer(all_rooms, many=True)
+        serializer = RoomListSerializer(
+            all_rooms,
+            many=True,
+            context={"request": request},
+        )
         return Response(serializer.data)
 
     def post(self, request):
@@ -120,5 +124,14 @@ class RoomDetail(APIView):
 
     def get(self, request, pk):
         room = self.get_object(pk)
-        serializer = RoomDetailSerializer(room)
+        serializer = RoomDetailSerializer(
+            room,
+            context={"request": request},
+        )
         return Response(serializer.data)
+
+    # def put(self, request, pk):
+    #     room = self.get_object(pk)
+    #     if not request.user.is_authenticated:
+    #         raise NotAuthenticated
+    #     if room.owner != request.user:
