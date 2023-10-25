032749a8bcb486e619c41a1a203690c4065597a7
watching-dev <coderefactoring90@gmail.com>
2023-10-25 11:07:55 +0900

    [feat] uploads 관련 다시 마이그레이션, Upload Photo 추가

--- airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
@@ -2,12 +2,18 @@ from django.conf import settings
 from rest_framework.views import APIView
 from django.db import transaction
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
+from rest_framework.exceptions import (
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+    PermissionDenied,
+)
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Amenity, Room
 from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from reviews.serializers import ReviewSerializer
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -164,5 +170,22 @@ class RoomReviews(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if not request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.errors)
+        else:
+            return Response(serializer.errors)
