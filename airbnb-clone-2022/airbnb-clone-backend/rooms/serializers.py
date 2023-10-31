581a67d83463f720f38d76ba01a55485dfb13b7c
watching-dev <coderefactoring90@gmail.com>
2023-10-31 17:45:47 +0900

    [feat] is_liked 추가

--- airbnb-clone-2022/airbnb-clone-backend/rooms/serializers.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/serializers.py
@@ -5,6 +5,7 @@ from users.serializers import TinyUserSerializer
 from reviews.serializers import ReviewSerializer
 from categories.serializer import CategorySerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitySerializer(ModelSerializer):
@@ -30,6 +31,7 @@ class RoomDetailSerializer(ModelSerializer):
 
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_likerd = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -42,6 +44,13 @@ class RoomDetailSerializer(ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__pk=room.pk,
+        ).exists()
 
 
 class RoomListSerializer(ModelSerializer):
