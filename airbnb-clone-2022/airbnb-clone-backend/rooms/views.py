d1fff54e1199fa688e33a6dfcf596bc71169e444
watching-dev <coderefactoring90@gmail.com>
2023-10-31 18:48:14 +0900

    [feat] Create a Booking

--- airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/rooms/views.py
@@ -17,7 +17,7 @@ from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerial
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializer import PublicBookingSerializer
+from bookings.serializer import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Amenities(APIView):
@@ -236,3 +236,12 @@ class RoomBookings(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            check_in = request.data.get("check_in")
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
