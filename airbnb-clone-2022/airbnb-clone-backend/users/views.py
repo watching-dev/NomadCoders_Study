2fd2ddf27d60c442b221277d207ea0884398ba2a
watching-dev <coderefactoring90@gmail.com>
2023-11-01 20:05:00 +0900

    [feat] LogIn and LogOut 추가

--- airbnb-clone-2022/airbnb-clone-backend/users/views.py
+++ airbnb-clone-2022/airbnb-clone-backend/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import ParseError, NotFound
@@ -75,3 +76,29 @@ class ChangePassword(APIView):
         else:
             # return Response(status=status.HTTP_400_BAD_REQUEST)
             return ParseError
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            usernmame=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+
+
+class LogOut(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye!"})
