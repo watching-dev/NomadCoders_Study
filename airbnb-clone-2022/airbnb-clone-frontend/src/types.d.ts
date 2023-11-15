f80584cc65c11c3e7482e2363a9fa5840e8f1e00
watching-dev <coderefactoring90@gmail.com>
2023-11-15 23:51:45 +0900

    [feat] Credentials (With B.E.)_server <-> client 쿠키로 연결해서 인증

--- airbnb-clone-2022/airbnb-clone-frontend/src/types.d.ts
+++ airbnb-clone-2022/airbnb-clone-frontend/src/types.d.ts
@@ -51,3 +51,16 @@ export interface IReview {
   user: IRoomOwner;
   payload: string;
 }
+
+export interface IUser {
+  last_login: string;
+  username: string;
+  email: string;
+  date_joined: string;
+  avatar: string;
+  name: string;
+  is_host: boolean;
+  gender: string;
+  language: string;
+  currency: string;
+}
