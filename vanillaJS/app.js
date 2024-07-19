5f75c55673db3e1d6fd5be063b4c8b376f58d662
watching-dev <coderefactoring90@gmail.com>
2024-07-19 13:03:19 +0900

    [feat] event

--- vanillaJS/app.js
+++ vanillaJS/app.js
@@ -1,19 +1,10 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  console.dir(loginInput);
-  console.log(loginInput);
+function onLoginSubmit(event) {
+  // const username = loginInput.value;
+  event.preventDefault();
   console.log(loginInput.value);
-  const username = loginInput.value;
-  if (username === "") {
-    alert("Please name");
-  } else if (username.lesngth > 15) {
-    alert("long");
-  }
 }
-loginButton.addEventListener("click", onLoginBtnClick);
+
+loginForm.addEventListener("submit", onLoginSubmit);
